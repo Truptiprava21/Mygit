e584ec8b22b34be632602fe595a047742a55193c
Trupti <truptiprava06@gmail.com>
2022-01-24 17:10:57 +0530

    SFTP

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from fileinput import filename
+from msilib.schema import Directory
 from os import getcwd
 import pysftp
 from flask import Flask, json,jsonify,request
@@ -28,7 +29,7 @@ class test(Resource):
         with pysftp.Connection(host = myHostname ,port = 22, username=myUsername, password=myPassword, cnopts=cnopts) as sftp:
         # Connection = pysftp.Connection(host=myHostname ,port = 22, username=myUsername, password=myPassword, cnopts=cnopts)
             #print(pysftp.Connection)
-            print ("Connection succesfully stablished") 
+            print ("Connection succesfully stablished")                 
 
            
 
@@ -38,11 +39,34 @@ class test(Resource):
             
             with sftp.cd(remotepath='.'):
                 files = sftp.listdir()
+                directory= request.form['directory']
+                parent_dir = "C:/Users/Lenovo/OneDrive/Desktop/SFTP/"
+                path = os.path.join(parent_dir, directory)
+                print(path)
+                try:
+                    os.makedirs(path, exist_ok = True)
+                    print("Directory '%s' created successfully" % directory)
+                except OSError as error:
+                    print("Directory '%s' can not be created" % directory)
+              
+                try:
+                    os.chdir(path)
+                    print("Current working directory: {0}".format(os.getcwd()))
+                except FileNotFoundError:
+                    print("Directory: {0} does not exist".format(path))
+                except NotADirectoryError:
+                    print("{0} is not a directory".format(path))
+                except PermissionError:
+                    print("You do not have permissions to change to {0}".format(path))
+
+
                 for each_file in files:
-                    old_file = path.isfile(each_file)
+                    old_file = os.path.isfile(each_file)
                     if old_file is True:
                         print(each_file,"alredy exist")
                     elif (each_file[-4:]=='.txt','.mp3','.mp4'):
+                        # sftp.get(remotepath = '.', localpath=None, callback=None, preserve_mtime=False)
+                        
                         sftp.get(each_file)
                         print(each_file,' downloaded successfully ')
                 
@@ -51,10 +75,10 @@ class test(Resource):
 
 api.add_resource(test,'/test')
 
-    
+
+  
 if __name__ =='__main__':
     app.run(debug=True)
-
 
 
 
